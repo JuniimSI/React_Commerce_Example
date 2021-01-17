89bb6380601a942450f9ba552b2303944ebe4819
Francisco Fernandes Júnior <jrfernandesti@gmail.com>
2021-01-16 23:36:36 -0300

    cors

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set("view engine", "ejs")
 app.use(express.static('public'));
+// Enable CORS
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 
 const port = process.env.PORT || 5000;
 app.use(routes);
